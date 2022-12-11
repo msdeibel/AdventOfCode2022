b029bbc742f714ae5593f9b35102287c804d5e6d
Deibel, Markus <markus.deibel@sinc.de>
2022-12-11 19:53:10 +0100

    Solves 2022-12-10 Part1

---
+++ aoc2022.Test/Aoc20221210Tests.cs
@@ -0,0 +1,355 @@
+﻿namespace aoc2022.Test;
+
+public class Aoc20221210Tests
+{
+
+    [Fact]
+    public void ExampleItemsDetected()
+    {
+        var aoc20221210 = new Aoc20221210(Example1Input());
+
+        Assert.Equal(420, aoc20221210.Cycle2RegisterValue[20]);
+    }
+
+    [Fact]
+    public void ShortExampleItemsDetected()
+    {
+        var aoc20221210 = new Aoc20221210(@"noop
+addx 3
+addx -5");
+
+        Assert.Equal(1, aoc20221210.Cycle2RegisterValue[1]);
+        Assert.Equal(2, aoc20221210.Cycle2RegisterValue[2]);
+        Assert.Equal(3, aoc20221210.Cycle2RegisterValue[3]);
+        Assert.Equal(16, aoc20221210.Cycle2RegisterValue[4]);
+       // Assert.Equal(1, aoc20221210.Cycle2RegisterValue[5]);
+    }
+
+    [Fact]
+    public void AllSignalsInExample()
+    {
+        var aoc20221210 = new Aoc20221210(Example1Input());
+
+        int total = 0;
+
+        for (int i = 20; i < aoc20221210.Cycle2RegisterValue.Count; i += 40)
+        {
+            total += aoc20221210.Cycle2RegisterValue[i];
+        }
+
+        Assert.Equal(13140, total);
+    }
+
+
+    [Fact]
+    public void Part1PuzzleInput()
+    {
+        var aoc20221210 = new Aoc20221210(PuzzleInput());
+
+        int total = 0;
+
+        for (int i = 20; i < aoc20221210.Cycle2RegisterValue.Count; i += 40)
+        {
+            total += aoc20221210.Cycle2RegisterValue[i];
+        }
+
+        Assert.Equal(14760, total);
+    }
+
+    private string Example1Input()
+    {
+        return @"addx 15
+addx -11
+addx 6
+addx -3
+addx 5
+addx -1
+addx -8
+addx 13
+addx 4
+noop
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx -35
+addx 1
+addx 24
+addx -19
+addx 1
+addx 16
+addx -11
+noop
+noop
+addx 21
+addx -15
+noop
+noop
+addx -3
+addx 9
+addx 1
+addx -3
+addx 8
+addx 1
+addx 5
+noop
+noop
+noop
+noop
+noop
+addx -36
+noop
+addx 1
+addx 7
+noop
+noop
+noop
+addx 2
+addx 6
+noop
+noop
+noop
+noop
+noop
+addx 1
+noop
+noop
+addx 7
+addx 1
+noop
+addx -13
+addx 13
+addx 7
+noop
+addx 1
+addx -33
+noop
+noop
+noop
+addx 2
+noop
+noop
+noop
+addx 8
+noop
+addx -1
+addx 2
+addx 1
+noop
+addx 17
+addx -9
+addx 1
+addx 1
+addx -3
+addx 11
+noop
+noop
+addx 1
+noop
+addx 1
+noop
+noop
+addx -13
+addx -19
+addx 1
+addx 3
+addx 26
+addx -30
+addx 12
+addx -1
+addx 3
+addx 1
+noop
+noop
+noop
+addx -9
+addx 18
+addx 1
+addx 2
+noop
+noop
+addx 9
+noop
+noop
+noop
+addx -1
+addx 2
+addx -37
+addx 1
+addx 3
+noop
+addx 15
+addx -21
+addx 22
+addx -6
+addx 1
+noop
+addx 2
+addx 1
+noop
+addx -10
+noop
+noop
+addx 20
+addx 1
+addx 2
+addx 2
+addx -6
+addx -11
+noop
+noop
+noop";
+    }
+
+    private string PuzzleInput()
+    {
+        return @"addx 2
+addx 3
+noop
+noop
+addx 1
+addx 5
+addx -1
+addx 5
+addx 1
+noop
+noop
+addx 4
+noop
+noop
+addx 5
+addx -5
+addx 6
+addx 3
+addx 1
+addx 5
+addx 1
+noop
+addx -38
+addx 41
+addx -22
+addx -14
+addx 7
+noop
+noop
+addx 3
+addx -2
+addx 2
+noop
+addx 17
+addx -12
+addx 5
+addx 2
+addx -16
+addx 17
+addx 2
+addx 5
+addx 2
+addx -30
+noop
+addx -6
+addx 1
+noop
+addx 5
+noop
+noop
+noop
+addx 5
+addx -12
+addx 17
+noop
+noop
+noop
+noop
+addx 5
+addx 10
+addx -9
+addx 2
+addx 5
+addx 2
+addx -5
+addx 6
+addx 4
+noop
+noop
+addx -37
+noop
+noop
+addx 17
+addx -12
+addx 30
+addx -23
+addx 2
+noop
+addx 3
+addx -17
+addx 22
+noop
+noop
+noop
+addx 5
+noop
+addx -10
+addx 11
+addx 4
+noop
+addx 5
+addx -2
+noop
+addx -6
+addx -29
+addx 37
+addx -30
+addx 27
+addx -2
+addx -22
+noop
+addx 3
+addx 2
+noop
+addx 7
+addx -2
+addx 2
+addx 5
+addx -5
+addx 6
+addx 2
+addx 2
+addx 5
+addx -25
+noop
+addx -10
+noop
+addx 1
+noop
+addx 2
+noop
+noop
+noop
+noop
+addx 7
+addx 1
+addx 4
+addx 1
+noop
+addx 2
+noop
+addx 3
+addx 5
+addx -1
+noop
+addx 3
+addx 5
+addx 2
+addx 1
+noop
+noop
+noop
+noop";
+    }
+}
---
+++ aoc2022/Aoc20221210.cs
@@ -0,0 +1,70 @@
+﻿namespace aoc2022;
+
+public class Aoc20221210 : AocBase
+{
+    public int RegisterX { get; }
+
+    private readonly Dictionary<int, int> _cycle2RegisterValue;
+
+    public IReadOnlyDictionary<int, int> Cycle2RegisterValue => _cycle2RegisterValue;
+
+    public Aoc20221210(string rawInput)
+        : base(rawInput)
+    {
+        _cycle2RegisterValue = new Dictionary<int, int>();
+        RegisterX = 1;
+
+        var q = new Queue<Operation>();
+        //q.Enqueue(new Operation("noop"));
+
+        var cycle = 0;
+        var isAdding = false;
+        Operation op = null;
+
+        int instructionPointer = 0;
+
+        do
+        {
+            cycle++;
+
+            _cycle2RegisterValue[cycle] = RegisterX * cycle;
+
+            if (!isAdding)
+            {
+                // Start operation
+                op = new Operation(InputRows.ElementAt(instructionPointer));
+
+                if (op.IsAdd)
+                {
+                    isAdding = true;
+                }
+
+                instructionPointer++;
+            }
+            else
+            {
+                RegisterX += op.Value;
+                isAdding = false;
+            }
+        } while (instructionPointer < InputRows.Count());
+    }
+}
+
+public class Operation
+{
+    public string Command { get; private set; }
+
+    public int Value { get; private set; }
+
+    public bool IsAdd => Command.Equals("addx");
+
+    public Operation(string rawInput)
+    {
+        Command = rawInput.Split(' ')[0];
+
+        if (Command.Equals("addx"))
+        {
+            Value = int.Parse(rawInput.Split(' ')[1]);
+        }
+    }
+}
